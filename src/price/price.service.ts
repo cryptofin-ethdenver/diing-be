a859c559269867531599e57448f36a2849d4890c
tnoh <taehyeongn@gmail.com>
2023-03-05 18:31:50 +0900

    Add cache for coingecko API call

--- src/price/price.service.ts
+++ src/price/price.service.ts
@@ -3,11 +3,19 @@ import axios from 'axios';
 
 @Injectable()
 export class PriceService {
+  private cache: any;
+  private last: number;
+
   async fetchPrice() {
-    const coinList = 'ethereum,usd-coin,tether,matic-network';
-    const res = await axios.get(
-      `https://api.coingecko.com/api/v3/simple/price?ids=${coinList}&vs_currencies=usd`,
-    );
-    return res.data;
+    const now = Date.now();
+    if (this.last <= now - 60000 || !this.cache) {
+      const coinList = 'ethereum,usd-coin,tether,matic-network';
+      const res = await axios.get(
+        `https://api.coingecko.com/api/v3/simple/price?ids=${coinList}&vs_currencies=usd`,
+      );
+      this.cache = res.data;
+      this.last = now;
+    }
+    return this.cache;
   }
 }
